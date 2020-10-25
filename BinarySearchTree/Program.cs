91dd6122a7789da5c7d6ba31e42691ac56a3d7ca
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-10-26 00:14:01 +0530

    [Abhishek] Add . Added to the UC2 branch

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -9,6 +9,16 @@ namespace BinarySearchTree
             BinaryTree<int> tree = new BinaryTree<int>(56);
             tree.Insert(70);
             tree.Insert(30);
+            tree.Insert(40);
+            tree.Insert(22);
+            tree.Insert(11);
+            tree.Insert(16);
+            tree.Insert(3);
+            tree.Insert(60);
+            tree.Insert(95);
+            tree.Insert(65);
+            tree.Insert(63);
+            tree.Insert(67);
             tree.Display();
         }
     }
